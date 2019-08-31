e7243e344dcbb704af68c96210798c830baf6588
brittany rollins <bmrollin@usc.edu>
2019-08-31 11:52:18 -0700

    added git commands to file

--- helloworld.cpp
+++ helloworld.cpp
@@ -3,4 +3,39 @@
 int main() 
 {
   std::cout << "FTFO!" << std::endl;
+
+
+
+
+  /* notes
+  # Clone a repository
+git clone git@github.com:TTrojan/cs104-git-practice.git
+
+# Add an untracked or modified file
+git add <file1> <file2> <...>
+
+# Remove a file from your git repository AND your local repository
+git rm <file1> <file2> <...>
+
+# Commit files that are staged for commit
+git commit -m "non-optional commit message"
+
+# Push commits to the server
+git push
+
+# Check the status of the repo
+git status
+
+# Let's make sure our repository is up to date
+git pull
+
+# See the commit history
+git log
+
+# See the commit history by printing a summary of each commit in a line
+git log --pretty=oneline
+
+# See the log of the last commit (-1) showing diffs of all the files (-p)
+git log -p -1
+*/
 }
